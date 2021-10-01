2fe7f1f1fe418f9f2ba355a05e1da458d68cb100
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-09-30 18:32:34 -0600

    Add tests for arrange

--- tests/test_tidyframe.py
+++ tests/test_tidyframe.py
@@ -6,11 +6,11 @@ def test_arrange1():
     df = tf.tibble({'x': ['a', 'a', 'b'], 'y': [2, 1, 3]})
     expected = tf.tibble({'x': ['a', 'a', 'b'], 'y': [1, 2, 3]})
     actual = df.arrange('y')
-    assert actual == expected, "arrange ascending failed"
+    assert actual.frame_equal(expected), "arrange ascending failed"
 
 def test_arrange2():
     """Can arrange descending"""
     df = tf.tibble({'x': ['a', 'a', 'b'], 'y': [2, 1, 3]})
     expected = tf.tibble({'x': ['b', 'a', 'a'], 'y': [3, 1, 2]})
-    actual = df.arrange('x', 'y', desc = [False, True])
-    assert actual == expected, "arrange ascending failed"
+    actual = df.arrange('x', 'y', desc = [True, False])
+    assert actual.frame_equal(expected), "arrange descending failed"
