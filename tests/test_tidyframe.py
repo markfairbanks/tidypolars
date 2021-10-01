42b58ac7f6e1fe73e337ab8a77f1908ddbdbce72
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-09-30 18:28:13 -0600

    Add initial tests

--- tests/test_tidyframe.py
+++ tests/test_tidyframe.py
@@ -1,1 +1,16 @@
-from tidyframe import tidyframe
+import tidyframe as tf
+from tidyframe import col
+
+def test_arrange1():
+    """Can arrange ascending"""
+    df = tf.tibble({'x': ['a', 'a', 'b'], 'y': [2, 1, 3]})
+    expected = tf.tibble({'x': ['a', 'a', 'b'], 'y': [1, 2, 3]})
+    actual = df.arrange('y')
+    assert actual == expected, "arrange ascending failed"
+
+def test_arrange2():
+    """Can arrange descending"""
+    df = tf.tibble({'x': ['a', 'a', 'b'], 'y': [2, 1, 3]})
+    expected = tf.tibble({'x': ['b', 'a', 'a'], 'y': [3, 1, 2]})
+    actual = df.arrange('x', 'y', desc = [False, True])
+    assert actual == expected, "arrange ascending failed"
