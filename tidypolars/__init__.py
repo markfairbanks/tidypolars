3323809a3d8cccc8399c55c76d41f53dac85c0ab
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-10-13 15:08:41 -0600

    Implement case_when/if_else

--- tidypolars/__init__.py
+++ tidypolars/__init__.py
@@ -5,5 +5,6 @@ __version__ = version("tidypolars")
 from .tidypolars import *
 from .expr_funs import *
 from .reexports import *
+from .funs import *
 
-__all__ = tidypolars.__all__ + reexports.__all__
+__all__ = tidypolars.__all__ + reexports.__all__ + funs.__all__
