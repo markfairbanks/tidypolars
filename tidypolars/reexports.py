8f4063bb53d71eedcc439bab1eb98d5d8fdcff1e
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-10-14 10:12:15 -0600

    Reexport polars dtypes

--- tidypolars/reexports.py
+++ tidypolars/reexports.py
@@ -1,9 +1,35 @@
 import polars as pl
 
-__all__ = ['all', 'col', 'lit', 'Expr', 'Series']
+__all__ = [
+    'all', 'col', 'lit', 'Expr', 'Series',
+
+    # dtypes
+    'Int8', 'Int16', 'Int32', 'Int64',
+    'UInt8', 'UInt16', 'UInt32', 'UInt64',
+    'Float32', 'Float64', 'Boolean', 'Utf8',
+    'List', 'Date', 'Datetime', 'Object'
+]
 
 all = pl.all
 col = pl.col
 lit = pl.lit
 Expr = pl.Expr
 Series = pl.Series
+
+# dtypes
+Int8 = pl.Int8
+Int16 = pl.Int16
+Int32 = pl.Int32
+Int64 = pl.Int64
+UInt8 = pl.UInt8
+UInt16 = pl.UInt16
+UInt32 = pl.UInt32
+UInt64 = pl.UInt64
+Float32 = pl.Float32
+Float64 = pl.Float64
+Boolean = pl.Boolean
+Utf8 = pl.Utf8
+List = pl.List
+Date = pl.Date
+Datetime = pl.Datetime
+Object = pl.Object
