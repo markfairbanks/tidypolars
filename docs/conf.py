836669c94d42c73c06f4216662a7ec4f1f733e9b
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-10-18 14:52:55 -0600

    Hide utils. from docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,10 @@ autoapi_dirs = ["../tidypolars"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build", "Thumbs.db", ".DS_Store",
+    "tidypolars/utils.py"
+]
 
 # -- Options for HTML output -------------------------------------------------
 
