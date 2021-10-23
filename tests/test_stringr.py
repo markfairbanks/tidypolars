da3232d18d45eb492b4552162a09b98e07ce82c8
mjkarlsen <matthew.j.carlson00@gmail.com>
2021-10-23 04:32:55 -0500

    Add str_detetct tests

---
+++ tests/test_stringr.py
@@ -0,0 +1,25 @@
+import tidypolars as tp
+# ADD TP to all selects
+def _repeat(x, times):
+    if not isinstance(x, list):
+        x = [x]
+    return x * times
+
+def test_str_detect_single():
+    """Can str_detect find a single string"""
+    df = tp.Tibble(name = ['apple', 'banana', 'pear', 'grape'])
+    actual = df.mutate(single = tp.str_detect('name', ['a']), single_negate = tp.str_detect('name', ['a'], negate=True))
+    expected = tp.Tibble(name = ['apple', 'banana', 'pear', 'grape'], 
+                         single = [True, True, True, True], 
+                         single_negate = [False, False, False, False]
+                         )
+    assert actual.frame_equal(expected), "str_detect single failed"
+
+def test_str_detect_multiple():
+    """Can str_detect find multiple strings"""
+    df = tp.Tibble(name = ['apple', 'banana', 'pear', 'grape'])
+    actual = df.mutate(multiple = tp.str_detect('name', ['a', 'e']), multiple_negate = tp.str_detect('name', ['a', 'e'], negate=True))
+    expected = tp.Tibble(name = ['apple', 'banana', 'pear', 'grape'], 
+                        multiple = [True, False, True, True], 
+                        multiple_negate = [False, True, False, False])
+    assert actual.frame_equal(expected), "str_detect multiple failed"
