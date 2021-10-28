398afd5c762fab2dbcd355c99f6a6261a52c9bc6
mjkarlsen <matthew.j.carlson00@gmail.com>
2021-10-28 09:45:06 -0500

    Added null_true = True to test_full_join()

--- tests/test_tidypolars.py
+++ tests/test_tidypolars.py
@@ -107,13 +107,13 @@ def test_filter():
     expected = tp.Tibble({'x': range(2), 'y': range(2)})
     assert actual.frame_equal(expected), "filter failed"
 
-# def test_full_join():
-#     """Can perform a full join"""
-#     df1 = tp.Tibble(x = ['a', 'a', 'b'], y = range(3))
-#     df2 = tp.Tibble(x = ['a'], z = range(1))
-#     actual = df1.full_join(df2)
-#     expected = tp.Tibble(y = [0, 1, 2], x = ['a', 'a', 'b'], z = [0, 0, None])
-#     assert actual.frame_equal(expected), "full_join failed"
+def test_full_join():
+    """Can perform a full join"""
+    df1 = tp.Tibble(x = ['a', 'a', 'b'], y = range(3))
+    df2 = tp.Tibble(x = ['a'], z = range(1))
+    actual = df1.full_join(df2)
+    expected = tp.Tibble(y = [0, 1, 2], x = ['a', 'a', 'b'], z = [0, 0, None])
+    assert actual.frame_equal(expected, null_equal = True), "full_join failed"
 
 def test_inner_join():
     """Can perform a inner join"""
