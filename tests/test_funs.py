950d18e5bab684ecab311103c795090e296a92a5
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-11-18 09:14:52 -0700

    Add more expression functions

--- tests/test_funs.py
+++ tests/test_funs.py
@@ -15,6 +15,7 @@ def test_agg_stats():
     actual = (
         df
         .summarize(
+            count_x = tp.count('x'), count_col_x = tp.count(col('x')),
             first_x = tp.first('x'), first_col_x = tp.first(col('x')),
             last_x = tp.last('x'), last_col_x = tp.last(col('x')),
             max_x = tp.max('x'), max_col_x = tp.max(col('x')),
@@ -22,11 +23,13 @@ def test_agg_stats():
             median_x = tp.median('x'), median_col_x = tp.median(col('x')),
             min_x = tp.min('x'), min_col_x = tp.min(col('x')),
             n_distinct_x = tp.n_distinct('x'), n_distinct_col_x = tp.n_distinct(col('x')),
+            quantile_x = tp.quantile('x', .25),
             sd_x = tp.sd('x'), sd_col_x = tp.sd(col('x')),
             sum_x = tp.sum('x'), sum_col_x = tp.sum(col('x')),
         )
     )
     expected = tp.Tibble(
+        count_x = [3], count_col_x = [3],
         first_x = [0], first_col_x = [0],
         last_x = [2], last_col_x = [2],
         max_x = [2], max_col_x = [2],
@@ -34,6 +37,7 @@ def test_agg_stats():
         median_x = [1], median_col_x = [1],
         min_x = [0], min_col_x = [0],
         n_distinct_x = [3], n_distinct_col_x = [3],
+        quantile_x = [0],
         sd_x = [1], sd_col_x = [1],
         sum_x = [3], sum_col_x = [3],
     )
@@ -62,6 +66,13 @@ def test_casting():
                          int_cast = [1, 2, 3],
                          string_cast = ["1", "2", "3"])
     assert actual.frame_equal(expected), "case_when failed"
+
+def test_floor():
+    """Can get the floor"""
+    df = tp.Tibble(x = [1.1, 5.5])
+    actual = df.mutate(floor_x = tp.floor('x')).select('floor_x')
+    expected = tp.Tibble(floor_x = [1.0, 5.0])
+    assert actual.frame_equal(expected), "floor failed"
 
 def test_lag():
     """Can get lagging values with function"""
@@ -133,3 +144,10 @@ def test_round():
     actual = df.mutate(x = tp.round(col('x'), 1))
     expected = tp.Tibble(x = [1.1, 2.2, 3.3])
     assert actual.frame_equal(expected), "round failed"
+
+def test_sqrt():
+    """Can get the square root"""
+    df = tp.Tibble(x = [9, 25, 100])
+    actual = df.mutate(x = tp.sqrt('x'))
+    expected = tp.Tibble(x = [3, 5, 10])
+    assert actual.frame_equal(expected), "sqrt failed"
