6970e045dbe1846f78cfafae31c7ab080465610f
markfairbanks <mark.t.fairbanks@gmail.com>
2022-10-10 13:05:49 -0600

    Add tests

--- tests/test_lubridate.py
+++ tests/test_lubridate.py
@@ -1,6 +1,5 @@
 import tidypolars as tp
 from tidypolars import col
-import math
 
 def test_date():
     """Can do date operations"""
@@ -30,8 +29,13 @@ def test_date():
     )
     assert actual.frame_equal(expected), "date operations failed"
 
-def as_date_fmt():
+def test_as_date_fmt():
     """Can pass fmt to as_date"""
     df = tp.Tibble(date = ['12/31/2021'])
     out = df.mutate(date_parsed = tp.as_date(col('date'), fmt='%m/%d/%Y'))
-    assert out.pull().is_datetime(), "as_date fmt failed"
+    assert out.pull().is_datelike(), "as_date fmt failed"
+
+def test_make_date():
+    df = tp.Tibble(date = ['2021-12-1']).mutate(date = tp.as_date('date'))
+    out = df.mutate(date = tp.make_date(2021, 12, 1))
+    assert df.pull('date').series_equal(out.pull('date')), "make_date failed"
